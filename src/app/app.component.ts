60d681e90b9f0a2475f1c57347a9936da6086204
spudro228 <mompizzapls@mail.ru>
2017-06-19 21:30:20 +0300

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Tour of Heroes';
+  heroes: Heroe[] = HEROES;
+  selectedHero: Heroe;
+
+  onSelect(hero: Heroe){
+    this.selectedHero = hero;
+  }
+
 }
+
+export class Heroe {
+  id: number;
+  name: string
+  // constructor(parameters) {
+    
+  // }
+}
+const HEROES: Heroe[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MdToolbarModule, MdIconModule, MdButtonModule, MdCheckboxModule, MdSidenavModule, MaterialModule, MdNativeDateModule, MdMenuModule, MdCardModule } from '@angular/material';
+
+
 
 import { AppComponent } from './app.component';
+import { HttpModule } from "@angular/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MdButtonModule, 
+    MdCheckboxModule,
+    MdSidenavModule,BrowserModule,
+    MdNativeDateModule,
+    MdMenuModule,
+    MdToolbarModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    MdIconModule,
+    MdCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
